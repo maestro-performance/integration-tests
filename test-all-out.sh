82bb5b82504d7a59942fd89d8a32b380c134cd4d
Otavio R. Piske <angusyoung@gmail.com>
2018-11-10 16:30:16 +0100

    FIX: wrong network name for running the test

--- test-all-out.sh
+++ test-all-out.sh
@@ -4,7 +4,7 @@
 function testArtemis() {
     echo "Launching a SUT instance w/ Apache Artemis"
     docker-compose -f docker-compose.yml -f suts/docker-artemis-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="Artemis 2.6.3" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="Artemis 2.6.3" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-artemis-compose.yml down
     docker rmi -f maestro_sut  || true
@@ -16,7 +16,7 @@ function testIbmMqLight() {
     echo "Launching a SUT instance w/ IBM MQ Light"
     wget -c https://gist.githubusercontent.com/orpiske/43574edf7c6c3ef150550c70820c25b8/raw/21def540f911ac8bdbfe9bd899521e924a76c018/docker-ibmmqlight-compose.yml -O suts/docker-ibmmqlight-compose.yml
     docker-compose -f docker-compose.yml -f suts/docker-ibmmqlight-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_test_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="IBM MQ Light" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it -h maestro_test_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="IBM MQ Light" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-ibmmqlight-compose.yml down
     docker rmi -f maestro_sut  || true
@@ -28,7 +28,7 @@ function testIbmMqLight() {
 function testActiveMQ() {
     echo "Launching a SUT instance w/ Artemis"
     docker-compose -f docker-compose.yml -f suts/docker-activemq-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_test_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="ActiveMQ 5.15.2" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it -h maestro_test_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="ActiveMQ 5.15.2" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-activemq-compose.yml down
     docker rmi -f maestro_sut  || true
@@ -38,7 +38,7 @@ function testActiveMQ() {
 function testQpidDispatch() {
     echo "Launching a SUT instance w/ QPid Dispatch Router"
     docker-compose -f docker-compose.yml -f suts/docker-interconnect-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_test_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="Interconnect 1.4.0" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it -h maestro_test_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="Interconnect 1.4.0" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 90s
     docker-compose -f docker-compose.yml -f docker-interconnect-compose.yml down
     docker rmi -f maestro_sut  || true
@@ -50,7 +50,7 @@ function testQpidCpp() {
 
     docker build -t maestro_test_qpid ${localDir}/suts/qpid/
     docker-compose -f docker-compose.yml -f ${localDir}/suts/qpid/docker-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it --network=maestro_cluster -e PRODUCT_NAME="QPid CPP" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it --network=work_cluster -e PRODUCT_NAME="QPid CPP" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 90s
     docker-compose -f docker-compose.yml -f docker-interconnect-compose.yml down
     docker rmi -f maestro_sut  || true
@@ -63,7 +63,7 @@ function testRabbitMq() {
     docker build -t maestro_test_rabbitmq ${localDir}/suts/rabbitmq/
     docker-compose -f docker-compose.yml -f ${localDir}/suts/rabbitmq/docker-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
     sleep 10s
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="RabbitMQ" -e SEND_RECEIVE_URL="amqp://sut:5672/test.performance.queue?protocol=RABBITAMQP" maestro-test-client /usr/bin/test-runner-all-out.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="RabbitMQ" -e SEND_RECEIVE_URL="amqp://sut:5672/test.performance.queue?protocol=RABBITAMQP" maestro-test-client /usr/bin/test-runner-all-out.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-rabbitmq-compose.yml down
     # docker rmi -f maestro_sut  || true
--- test-fair-incremental.sh
+++ test-fair-incremental.sh
@@ -2,7 +2,7 @@
 function testIbmMqLight() {
     wget -c https://gist.githubusercontent.com/orpiske/43574edf7c6c3ef150550c70820c25b8/raw/21def540f911ac8bdbfe9bd899521e924a76c018/docker-ibmmqlight-compose.yml
     docker-compose -f docker-compose.yml -f docker-ibmmqlight-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="IBM MQ Light" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="IBM MQ Light" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-ibmmqlight-compose.yml down
     docker rmi -f maestro_sut || true
@@ -12,7 +12,7 @@ function testIbmMqLight() {
 # Artemis 2.6
 function testArtemis() {
     docker-compose -f docker-compose.yml -f docker-artemis-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="Artemis 2.6.3" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="Artemis 2.6.3" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-artemis-compose.yml down
     docker rmi -f maestro_sut || true
@@ -22,7 +22,7 @@ function testArtemis() {
 # ActiveMQ
 function testActiveMQ() {
     docker-compose -f docker-compose.yml -f docker-activemq-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="ActiveMQ 5.15.2" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="ActiveMQ 5.15.2" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-activemq-compose.yml down
     docker rmi -f maestro_sut || true
@@ -32,7 +32,7 @@ function testActiveMQ() {
 # Interconnect
 function testQpidDispatch() {
     docker-compose -f docker-compose.yml -f docker-interconnect-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="Interconnect 1.4.0" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="Interconnect 1.4.0" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 90s
     docker-compose -f docker-compose.yml -f docker-interconnect-compose.yml down
     docker rmi -f maestro_sut || true
@@ -44,7 +44,7 @@ function testQpidCpp() {
 
     docker build -t maestro_test_rabbitmq ${localDir}/docker/suts/rabbitmq/
     docker-compose -f docker-compose.yml -f ${localDir}/docker/suts/qpid/docker-compose.ymlup --scale worker=2 --scale agent=0 --scale inspector=0 -d
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="Qpid CPP" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="Qpid CPP" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 90s
     docker-compose -f docker-compose.yml -f docker-interconnect-compose.yml down
     docker rmi -f maestro_sut || true
@@ -58,7 +58,7 @@ function testRabbitMq() {
     docker build -t maestro_test_rabbitmq ${localDir}/docker/suts/rabbitmq/
     docker-compose -f docker-compose.yml -f ${localDir}/docker/suts/rabbitmq/docker-compose.yml up --scale worker=2 --scale agent=0 --scale inspector=0 -d
     10s
-    docker run -it -h maestro_client -v maestro:/maestro --network=maestro_cluster -e PRODUCT_NAME="RabbitMQ" -e SEND_RECEIVE_URL="amqp://sut:5672/test.performance.queue?protocol=RABBITAMQP" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
+    docker run -it -h maestro_client -v maestro:/maestro --network=work_cluster -e PRODUCT_NAME="RabbitMQ" -e SEND_RECEIVE_URL="amqp://sut:5672/test.performance.queue?protocol=RABBITAMQP" maestro-test-client /usr/bin/test-runner-fair-incremental.sh
     sleep 30s
     docker-compose -f docker-compose.yml -f docker-rabbitmq-compose.yml down
     docker rmi -f maestro_sut || true
