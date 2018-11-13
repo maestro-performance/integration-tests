f5044e805880dc526787ec527e141e18f26e0e7e
Otavio Rodolfo Piske <angusyoung@gmail.com>
2018-11-13 11:33:47 +0100

    UPD: to support extra SEND_RECEIVE_URL parameters

--- docker/test-runner-all-out.sh
+++ docker/test-runner-all-out.sh
@@ -8,6 +8,10 @@ export TEST_DURATION=5m
 export TEST_DESCRIPTION="${PRODUCT_NAME} test (AMQP) w/ ${MESSAGE_SIZE} bytes message (all-out)"
 export TEST_XUNIT_DIR=/maestro/tests/results
 
+if [[ ! -z "${SEND_RECEIVE_URL_OPTS}" ]] ; then
+    export SEND_RECEIVE_URL="${SEND_RECEIVE_URL}?${SEND_RECEIVE_URL_OPTS}"
+fi
+
 echo "Running ${TEST_DESCRIPTION}"
 /opt/maestro/maestro-cli/bin/maestro-cli maestro -m ${MAESTRO_BROKER} -c ping
 /opt/maestro/maestro-cli/bin/maestro-cli exec -s /opt/maestro/maestro-cli/scripts/singlepoint/FixedRateTest.groovy
