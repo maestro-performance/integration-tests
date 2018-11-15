8c8bbcdccf608519323251cf1d6589ade4c3d0c4
Otavio Rodolfo Piske <angusyoung@gmail.com>
2018-11-15 14:06:15 +0100

    UPD: ignore if the images don't exist

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ cd work
 cp ../test*.sh .
 cp ../docker*.yml .
 git clone https://github.com/maestro-performance/maestro-java.git -b devel
-make -f maestro-java/extra/docker-compose/maestro/Makefile clean
+make -f maestro-java/extra/docker-compose/maestro/Makefile clean || true
 mkdir -p suts
 cd suts &&  cp -Rv ../maestro-java/extra/docker-compose/maestro/suts/* .
 cp -Rv ../../suts/* .
