27a8e546e00a389b551a4cc44e8aef6734171fc3
Otavio Rodolfo Piske <angusyoung@gmail.com>
2018-11-15 14:05:12 +0100

    UPD: to clean the previous Maestro images

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ cd work
 cp ../test*.sh .
 cp ../docker*.yml .
 git clone https://github.com/maestro-performance/maestro-java.git -b devel
+make -f maestro-java/extra/docker-compose/maestro/Makefile clean
 mkdir -p suts
 cd suts &&  cp -Rv ../maestro-java/extra/docker-compose/maestro/suts/* .
 cp -Rv ../../suts/* .
