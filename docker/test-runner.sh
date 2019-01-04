e8e806323513f6fca7a60a4c2062d0ef58306591
Otavio R. Piske <angusyoung@gmail.com>
2019-01-04 10:03:42 +0100

    UPD: to display some variables at the beginning of the test

--- docker/test-runner.sh
+++ docker/test-runner.sh
@@ -4,11 +4,20 @@ function cleanup() {
     /opt/maestro/maestro-cli/bin/maestro-cli maestro -m ${MAESTRO_BROKER} -c stop
 }
 
-source /etc/maestro/test-profiles/$1.conf
+if [[ ! -f /etc/maestro/test-profiles/$1.conf ]] ; then
+    echo "The test configuratoin file is missing. Running with default parameters"
+else
+    source /etc/maestro/test-profiles/$1.conf
+fi
+
 
 if [[ ! -z "${SEND_RECEIVE_URL_OPTS}" ]] ; then
     export SEND_RECEIVE_URL="${SEND_RECEIVE_URL}?${SEND_RECEIVE_URL_OPTS}"
 fi
+
+echo "########### ENV START ###########"
+env 
+echo "########### ENV END ###########"
 
 echo "Running ${TEST_DESCRIPTION}"
 /opt/maestro/maestro-cli/bin/maestro-cli maestro -m ${MAESTRO_BROKER} -c ping
