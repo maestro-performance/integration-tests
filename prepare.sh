cad88d79202b432d22a492d666f2e6baf2bfba1b
Otavio R. Piske <angusyoung@gmail.com>
2019-01-06 09:40:53 +0100

    UPD: test preparation cleanups

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+
 mkdir -p work/results/{incremental,all-out}
 cd work
 cp ../test*.sh .
 cp ../docker*.yml .
+
+echo "Cleaning Maestro edge images"
+docker rmi -f $(docker images 'maestroperf/*' -q) || true
+
+
+echo "Preparing work directories and copying Maestro composer files"
 git clone https://github.com/maestro-performance/maestro-java.git -b devel
-make -f maestro-java/extra/docker-compose/maestro/Makefile clean || true
+
+
 mkdir -p suts
 cd suts &&  cp -Rv ../maestro-java/extra/docker-compose/maestro/suts/* .
 cp -Rv ../../suts/* .
+
+echo "Building new images"
 cd ../../docker
 make
 
